6ca04a1bfd05ac7cec47f95b6eefce3ddf73ec85
Julian Beck <ju-fa-beck@t-online.de>
2019-02-23 18:47:46 +0100

    minor changes

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
 const fs = require('fs');
 const path = require("path")
 const Table = require('cli-table3');
-const progress = require('cli-progress');
 const meow = require('meow');
-const formats = [".wav", ".mp3", ".mp4", ".flac", ".ogg"]
-
+const formats = [".wav", ".mp3", ".mp4", ".flac", ".ogg", ".mp3", ".mkv", ".mp4", ".avi", ".wmv", ".mov", ".amv", ".mpg", ".flv"]
+const chalk = require('chalk');
+const ora = require("ora");
 const cli = meow(`
     Usage
       $ cliconcat <input> <output file>
@@ -18,7 +18,8 @@ const cli = meow(`
       -o, --output name of the output file
  
     Examples
-      $ cliconcat 1.mp3 2.mp3 3.mp3 -o /pathto/output.mp3
+      $ cliconcat 1.mp3 2.mp3 3.mp3 -o /path/to/output.mp3
+      $ cliconcat -f /audiofolder -o /path/to/output.mp3
 `, {
     flags: {
         folder: {
@@ -32,56 +33,50 @@ const cli = meow(`
     }
 });
 
-(async () => {
-    ffmpeg.setFfmpegPath(ffmpegPath);
-    cli.input[0], cli.flags
-    let files
-    if (cli.flags.folder) {
-        if (cli.input[0] === undefined) {
-            console.log("Please specify a Folder");
-            cli.showHelp();
-            return;
-        }
-        files = fs.readdirSync(cli.input[0]).sort()
-    } else {
-        if(cli.input.length<2){
-            console.log("Please specify at least two media File");
-            cli.showHelp();
-            return;
-        }
-        cli.input.forEach(file => files.push(file));
+ffmpeg.setFfmpegPath(ffmpegPath);
+let files = [];
+if (cli.flags.folder) {
+    if (cli.input[0] === undefined) {
+        console.log("Please specify a Folder");
+        cli.showHelp();
+        return;
     }
-    if (cli.flags.output === undefined) {
-        console.log("Please specify an output folder")
-        cli.showHelp()
-        return
+    files = fs.readdirSync(cli.input[0]).map((file) => {
+        return path.join(cli.input[0], file)
+    }).sort()
+} else {
+    if (cli.input.length < 2) {
+        console.log(chalk.red("Please specify at least two media File"));
+        cli.showHelp();
+        return;
     }
-    let output = cli.flags.output
-    
-    let table = new Table({
-        head: ['Order:', 'Files:']
-    });
-    //Format for Table
-    files.map((el, i) => {
-        return [i + 1, el]
-    }).forEach((el) => table.push(el))
+    console.log(cli.input)
+    cli.input.forEach(file => files.push(file));
+}
+if (cli.flags.output === undefined) {
+    console.log(chalk.red("Please specify an output folder"));
+    cli.showHelp()
+    return
+}
+let output = cli.flags.output
+console.log(output)
+let table = new Table({
+    head: ['Order:', 'Files:']
+});
+files = files.filter(el => formats.includes(path.extname(el)))
 
-    const bar = new progress.Bar({
-        format: 'progress [{bar}] {percentage}% | ETA: {eta}s |'
-    });
+//Format for Table
+files.map((el, i) => {
+    return [i + 1, el]
+}).forEach((el) => table.push(el))
 
-    bar.start(100, 0);
-
-    files = files.map((file) => {
-        return path.join(folder, file)
+console.log(table.toString())
+const spinner = ora(`Merging ${files.length} Files`).start();
+files.reduce((prev, curr) => prev.input(curr), ffmpeg())
+    .on("error", err => {
+        spinner.fail(`Finished merging ${files.length} Files`)
+        console.log(err)
     })
-    files.reduce((prev, curr) => prev.input(curr), ffmpeg())
-        .on("error", err => console.log(`An error occurred while merging video files: ${err.message}`))
-        .on("end", () => {
-            console.log(`Finished merging ${files.length} Files`)
-        })
-        .on('progress', function (progress) {
-            bar.update(progress.percent)
-
-        }).mergeToFile(output);
-})();
+    .on("end", () => {
+        spinner.succeed(`Finished merging ${files.length} Files`)
+    }).mergeToFile(output);
