66410415e0fe050c596863af8e28e96dd5026373
Julian Beck <ju-fa-beck@t-online.de>
2019-02-23 17:51:39 +0100

    basic console functions

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
+#!/usr/bin/env node
+
 const ffmpeg = require("fluent-ffmpeg");
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
-const ffprobePath = require("@ffprobe-installer/ffprobe").path;
 const fs = require('fs');
 const path = require("path")
 const Table = require('cli-table3');
 const progress = require('cli-progress');
+const meow = require('meow');
+const formats = [".wav", ".mp3", ".mp4", ".flac", ".ogg"]
 
-
-
-
+const cli = meow(`
+    Usage
+      $ cliconcat <input> <output file>
+ 
+    Options
+      -f, --folder join all audio in folder alphabetically
+      -o, --output name of the output file
+ 
+    Examples
+      $ cliconcat 1.mp3 2.mp3 3.mp3 -o /pathto/output.mp3
+`, {
+    flags: {
+        folder: {
+            type: 'boolean',
+            alias: '-f'
+        },
+        output: {
+            type: 'string',
+            alias: '-o'
+        }
+    }
+});
 
 (async () => {
     ffmpeg.setFfmpegPath(ffmpegPath);
-    ffmpeg.setFfprobePath(ffprobePath);
-    let folder = "/Users/julianbeck/Desktop/Book 04 - Harry Potter and the Goblet of Fire"
-    let files = fs.readdirSync(folder).sort()
-    let output = path.join(folder, "finished.mp3")
-    let temp = path.join(folder, "temp")
+    cli.input[0], cli.flags
+    let files
+    if (cli.flags.folder) {
+        if (cli.input[0] === undefined) {
+            console.log("Please specify a Folder");
+            cli.showHelp();
+            return;
+        }
+        files = fs.readdirSync(cli.input[0]).sort()
+    } else {
+        if(cli.input.length<2){
+            console.log("Please specify at least two media File");
+            cli.showHelp();
+            return;
+        }
+        cli.input.forEach(file => files.push(file));
+    }
+    if (cli.flags.output === undefined) {
+        console.log("Please specify an output folder")
+        cli.showHelp()
+        return
+    }
+    let output = cli.flags.output
+    
     let table = new Table({
         head: ['Order:', 'Files:']
     });
@@ -28,7 +69,7 @@ const progress = require('cli-progress');
     const bar = new progress.Bar({
         format: 'progress [{bar}] {percentage}% | ETA: {eta}s |'
     });
-    
+
     bar.start(100, 0);
 
     files = files.map((file) => {
@@ -36,11 +77,11 @@ const progress = require('cli-progress');
     })
     files.reduce((prev, curr) => prev.input(curr), ffmpeg())
         .on("error", err => console.log(`An error occurred while merging video files: ${err.message}`))
-        .on("end", () => {console.log(`Finished merging ${files.length} Files`)})
+        .on("end", () => {
+            console.log(`Finished merging ${files.length} Files`)
+        })
         .on('progress', function (progress) {
             bar.update(progress.percent)
 
-        })
-
-        .mergeToFile(output);
+        }).mergeToFile(output);
 })();
